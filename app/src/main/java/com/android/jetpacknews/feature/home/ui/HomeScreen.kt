5994dae17cbbb61f79a5e30a5b17c6b3b6c433f8
Preetham Dsouza <preetham.dsouza@ymedialabs.com>
2022-06-23 14:01:33 +0530

    [Fix] Resolved tab data duplication
    - Same first page data used to load and on every tab click no API was gettingcalled, resolved the same using the snapshotFlow

--- app/src/main/java/com/android/jetpacknews/feature/home/ui/HomeScreen.kt
+++ app/src/main/java/com/android/jetpacknews/feature/home/ui/HomeScreen.kt
@@ -1,17 +1,22 @@
 package com.android.jetpacknews.feature.home.ui
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.*
 import androidx.compose.material.MaterialTheme.typography
+import androidx.compose.material.ScrollableTabRow
+import androidx.compose.material.Tab
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.android.jetpacknews.feature.home.presentation.viewmodel.HomeScreenViewModel
 import com.android.jetpacknews.ui.theme.JetPackNewsTheme
 import com.google.accompanist.pager.ExperimentalPagerApi
 import com.google.accompanist.pager.HorizontalPager
@@ -53,15 +58,23 @@ fun HomeScreen() {
             }
             HorizontalPager(
                 count = tabData.size,
-                state = pagerState,
-                modifier = Modifier.weight(1f),
-            ) { index ->
+                state = pagerState
+            ) {
+                val viewModel: HomeScreenViewModel = hiltViewModel()
+                // Added to get the exact clicked tab data else all the tabs will be notified
+                // Ref: https://google.github.io/accompanist/pager/#reacting-to-page-changes
+                LaunchedEffect(pagerState) {
+                    snapshotFlow { pagerState.currentPage }.collect { page ->
+                        viewModel.updateQuery(tabData[page])
+                        viewModel.loadData()
+                    }
+                }
                 Column(
                     modifier = Modifier.fillMaxSize(),
                     verticalArrangement = Arrangement.Center,
                     horizontalAlignment = Alignment.CenterHorizontally
                 ) {
-                    TabScreen(tabData[index])
+                    TabScreen(viewModel)
                 }
             }
         }
